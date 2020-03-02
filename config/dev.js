911e4e64aafd14c8298c311a9d551fc1b93eff40
chekai7 <45897779+chekai7@users.noreply.github.com>
2020-03-02 22:08:33 +0800

    oauth finish 0302

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+//development keys(dev开发版本)
+module.exports = {
+  googleClientId: '31927844005-c0e1nmfsj2h0b0hvfob1qjtr14g5m8dg.apps.googleusercontent.com',
+  googleClientScret: '3F0IDwHk3P3rkakY6cJcWzgb',
+  mongoURI: 'mongodb+srv://kai:Queen75321m@email-mrsuo.mongodb.net/test?retryWrites=true&w=majority',
+  cookieKey: 'redacted',
+}
